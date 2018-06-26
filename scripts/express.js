3dff61d61f82acca8c818f79ae2f23c5d544e5cb
bezalel <bezalelp@uci.edu>
2018-06-26 07:55:29 -0400

    saving before editing edit portion

--- scripts/express.js
+++ scripts/express.js
@@ -19,102 +19,168 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-// app.use(express.static('style'));
-// app.use(express.static(path.join(__dirname, '../style')));
 app.use(express.static('images'));
 app.use(express.static('style'));
 app.use(express.static('scripts'));
-
-
-
-
-
-// testing db CRUD
-var jsObjList = { 
-  biology:
-  { card1: [ 'mitochondira', 'powerhouse of cell' ],
-    card2: [ 'dopamine', 'neurotransmitter' ],
-    card3: [ 'phalange', 'handsies or footsies' ] },
-  stock:
-  { card1: [ 'ROI', 'return on investment' ],
-    card2: [ 'PDT', 'pattern day trader = nono' ],
-    card3: [ 'robinhood', 'hit me up for a free stock =]' ] },
-  instrument:
-  { card1: [ 'mayonnaise', 'no Patrick' ],
-    card2: [ 'contrabass tuba', 'REALLY big tuba' ],
-    card3: [ 'symbols', 'who really knows' ] } };
-
-var newCard =  {card4: [ 'anotha', 'less get et' ]};
-
-function getSetNameList (obj) {
-  var keyList = [];
-  for (var key in obj) {
-    keyList.push(key);
+//mongoDB
+var ObjectId = require('mongodb').ObjectID;
+const mongodb = require('mongodb').MongoClient;
+var db;
+mongodb.connect('mongodb://localhost:27017', (error, client) => {
+  if (error) {
+    console.error(error);
+    client.close();
   }
-  return keyList;
+  db = client.db('xflashcard');
+  app.listen(8000, () => {
+    console.log('listening on 8k');
+  });
+});
+
+// find stuff in db
+function dbInfo (res) {
+  db.collection('xflashcard').find({}).toArray((error, results) => {
+    if (error) {
+      return console.log(error);
+    }
+    // console.log('results: ' + results[0]._id);
+    res.render('display', {
+      xarray: results,
+    });
+  });
 }
-var xtest = getSetNameList(jsObjList);
-// console.log(getSetNameList(jsObjList));
-//returns [ 'biology', 'stock', 'instrument' ]
-
-function getCardsInSet(setName, obj) {
-  var cardList = [];
-  var newObj = obj[setName];
-  for (var key in newObj) {
-    cardList.push(newObj[key]);
-  }
-  return cardList;
-}
-// console.log(getCardsInSet('biology', jsObjList));
-//returns list of list 
-// [ [ 'mitochondira', 'powerhouse of cell' ],
-//   [ 'dopamine', 'neurotransmitter' ],
-//   [ 'phalange', 'handsies or footsies' ] ]
 
 
-
-
-
-
-
-
-
-
+//routes
 app.route('/')
   .get((req, res) => {
-    // console.log(jsObjList);
-    res.render('display', {
-      setChoice: getSetNameList(jsObjList),
-
+    db.collection('xflashcard').find({}).toArray((error, results) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('results-: ' + results);
+      res.render('display', {
+        xarray: results,
+        setname: req.body.setName,
+      });
     });
   })
   .post((req, res) => {
-    // console.log(req.body);
-    res.render('display', {
-      setChoice: getSetNameList(jsObjList),
-      setName: req.body.setName || req.body.setChoice, 
-      cardTerm: req.body.cardTerm,
-      cardDefinition: req.body.cardDefinition,
+    console.log(req.body);
+    db.collection('xflashcard').insertOne(req.body, (err, results) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      db.collection('xflashcard').find({}).toArray((error, results) => {
+        if (error) {
+          return console.log(error);
+        }
+        res.render('display', {
+          xarray: results,
+          setname: req.body.setName,
+        });
+      });
     });
-    // res.end();
   });
 
-// app.get('/', function (req, res) {
-//   //  res.send('Hello World');
-//   res.sendFile(path.join(__dirname, '../index.html'));
-//   // res.sendFile(path.join(__dirname, '../style/index.css'));
-//   // res.sendFile(path.join(__dirname, '../images/bp_logo6.png'));
-// })
+
+app.route('/delete/:id')
+  .post((req, res) => {
+    console.log(req.body.deleteItem);
+    // console.log('~');
+    // console.log(req.body.pname);
+    // console.log('~~');
+    // console.log(req.body._id);
+
+    var query = { _id: ObjectId(req.body.deleteItem.toString()) };
+    db.collection("xflashcard").deleteOne(query, function(err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted");
+      dbInfo(res);
+    });
+  });
 
 
-app.listen(8000);
+app.route('/edit/:id')
+  .get((req, res) => {
+    console.log(req.query.editItem);
+    var query = { _id: ObjectId(req.query.editItem.toString()) };
+    db.collection('xflashcard').findOne(query, (error, result) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('edit-results-: ' + result);
+      console.log(result);
+      res.render('edit', {
+        // xarray: results,
+        // setname: req.body.setName,
+        xarrayedit: result,
+      });
+    });
+  })
+  .post((req, res) => {
+    console.log(req.body.editItem);
+    console.log('~');
+    console.log(req.body);
+    console.log('~~');
+    console.log(req.body._id);
 
-// var server = app.listen(8081, function () {
-//    var host = server.address().address
-//    var port = server.address().port
-   
-//    console.log("Example app listening at http://%s:%s", host, port)
-// })
+    var findId = { _id: ObjectId(req.body._id.toString()) };
+    var oldValue = db.collection("xflashcard").findOne(findId, function(err, result) {
+      if (err) throw err;
+      console.log('@@');
+      console.log(result);
+      // return result;
+      // db.close();
+    }).then(() => {
+      var query = req.body;
+      // var query = { _id: ObjectId(req.body._id.toString()) };
+      console.log(query);
+      console.log('~~query');
+      var newValue = { $set: query };
+      db.collection("xflashcard").updateOne(oldValue, newValue, function(err, obj) {
+        if (err) throw err;
+        console.log("x1 document edited");
+        dbInfo(res);
+      });
+    });
+
+    // var query = req.body;
+    // // var query = { _id: ObjectId(req.body._id.toString()) };
+    // console.log(query);
+    // console.log('~~query');
+    // var newValue = { $set: query };
+    // db.collection("xflashcard").updateOne(oldValue, newValue, function(err, obj) {
+    //   if (err) throw err;
+    //   console.log("x1 document edited");
+    //   dbInfo(res);
+    // });
+  });
+
+
+
+// app.route('/')
+//   .get((req, res) => {
+//     // console.log(jsObjList);
+//     res.render('display', {
+//       setChoice: getSetNameList(jsObjList),
+
+//     });
+//   })
+//   .post((req, res) => {
+//     // console.log(req.body);
+//     res.render('display', {
+//       setChoice: getSetNameList(jsObjList),
+//       setName: req.body.setName || req.body.setChoice, 
+//       cardTerm: req.body.cardTerm,
+//       cardDefinition: req.body.cardDefinition,
+//     });
+//     // res.end();
+//   });
+
+
+
 
 
 
